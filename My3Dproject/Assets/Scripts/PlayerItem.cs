94d0c5a810e625cac836d52d9f939a280681bd1d
nero317 <jho20455744@gmail.com>
2024-05-02 01:30:04 +0900

    아이템 추가

---
+++ My3Dproject/Assets/Scripts/PlayerItem.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerItem : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public bool[] hasWeapons;
+
+    GameObject nearObject;
+    GameObject equipWeapon;
+    PlayerMovement pm;
+
+    int equipWeaponIndex = -1;
+
+    void Awake()
+    {
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    public void Interaction()
+    {
+        if (pm.iDown && nearObject != null)
+        {
+            Debug.Log("YES");
+            if (nearObject.tag == "Weapon")
+            {
+                Item item = nearObject.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapons[weaponIndex] = true;
+
+                Destroy(nearObject);
+            }
+        }
+    }
+
+    public void Swap()
+    {
+        if (pm.sDown1 && (!hasWeapons[0] || equipWeaponIndex == 0)) return;
+        if (pm.sDown2 && (!hasWeapons[1] || equipWeaponIndex == 1)) return;
+        if (pm.sDown3 && (!hasWeapons[2] || equipWeaponIndex == 2)) return;
+
+        int weaponIndex = -1;
+        if (pm.sDown1) weaponIndex = 0;
+        if (pm.sDown2) weaponIndex = 1;
+        if (pm.sDown3) weaponIndex = 2;
+
+        if ((pm.sDown1 || pm.sDown2 || pm.sDown3) && !pm.isJump && !pm.isSwap)
+        {
+            Debug.Log("Swap!");
+            if (equipWeapon != null)
+                equipWeapon.SetActive(false);
+
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex];
+            equipWeapon.SetActive(true);
+
+            pm.anim.SetTrigger("doSwap");
+
+            pm.isSwap = true;
+            Invoke("SwapOut", 0.4f);
+        }
+    }
+
+    public void SwapOut()
+    {
+        pm.isSwap = false;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = other.gameObject;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = null;
+    }
+}
--- My3Dproject/Assets/Scripts/PlayerMovement.cs
+++ My3Dproject/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed, jumpForce;
-
     float hAxis, vAxis;
 
-    public bool wDown, jDown, isJump;
+    public bool jDown, isJump, isDodge, iDown;
+    public bool sDown1, sDown2, sDown3, isSwap;
 
-    public Vector3 moveVec;
+    public Vector3 moveVec, dodgeVec;
+
+    public Animator anim;
+
+    PlayerItem pi;
 
     Rigidbody rigid;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        pi = GetComponent<PlayerItem>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -24,14 +30,20 @@ public class PlayerMovement : MonoBehaviour
         GetInput();
         Move();
         Jump();
+        Dodge();
+        pi.Interaction();
+        pi.Swap();
     }
 
     void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal"); //x방향 입력 (키보드 a,d)
         vAxis = Input.GetAxisRaw("Vertical"); //z방향 입력 (키보드 w,s)
-        wDown = Input.GetButton("Walk"); //걷기 입력 (왼쪽 shift)
-        jDown = Input.GetButton("Jump"); //점프 입력
+        jDown = Input.GetButtonDown("Jump"); //점프 입력
+        iDown = Input.GetButtonDown("Interaction");
+        sDown1 = Input.GetButtonDown("Swap1");
+        sDown2 = Input.GetButtonDown("Swap2");
+        sDown3 = Input.GetButtonDown("Swap3");
     }
 
     void Move()
@@ -39,27 +51,52 @@ public class PlayerMovement : MonoBehaviour
         //백터 생성, 대각선 이동때도 같은 값 가지기 위해 nomalized사용
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        if (wDown) //wDown == true라면
-            transform.position += moveVec * speed * 0.3f * Time.deltaTime; //0.3을 곱함(더 느리게)
-        else //wDown == false라면
-            transform.position += moveVec * speed * Time.deltaTime; //백터 * 스피드 * 델타타임을 위치에 계속 더해줌
+        if (isDodge) //회피중일경우
+            moveVec = dodgeVec; //moveVec를 dodgeVec으로 고정
+        if (isSwap)
+            moveVec = Vector3.zero;
 
-        transform.LookAt(transform.position + moveVec);
+        transform.position += moveVec * speed * Time.deltaTime; //백터 * 스피드 * 델타타임을 위치에 계속 더해줌
+
+        transform.LookAt(transform.position + moveVec); //플레이어가 이동방향을 바라봄
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
     }
 
     void Jump()
     {
-        if (jDown && !isJump)
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge) //점프버튼이 눌리고 점프중이 아닐경우
         {
-            rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isJump = true;
+            rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //위쪽벡터 * jumpForce만큼 힘을 줌
+            anim.SetBool("isJump", true); //애니메이터의 sJump값을 true로 변경
+            anim.SetTrigger("doJump"); //doJump 트리거 동작
+            isJump = true; //isJump값 true 변경
         }
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge) //점프버튼이 눌리고
+        {
+            dodgeVec = moveVec; //회피중 벡터를 회피직전의 벡터로 고정
+            speed *= 2; //속도 2배
+            anim.SetTrigger("doDodge"); //doDodge 트리거 동작
+            isDodge = true; //isDodge true로 변경
+            Invoke("DodgeOut", 0.4f); //0.4초후 DodgeOut함수 호출
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f; //속도 0.5배
+        isDodge = false; //isDodge false로 변경
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Floor")
         {
+            anim.SetBool("isJump", false);
             isJump = false;
         }
     }
